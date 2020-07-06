44ec9f9c79938135333bbf8e157f35ecfd58110d
pclwillmott <pwillmott@northrock.bm>
2020-07-05 21:15:51 -0300

    Initial Commit

---
+++ MyChartBox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyChartBox
+//
+//  Created by Paul Willmott on 05/07/2020.
+//  Copyright © 2020 Paul Willmott. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
